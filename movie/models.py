39201064e90c887184978afd2e41ea226fd7095d
Mark Kyama <mark.kyama.adal@gmail.com>
2022-08-01 14:26:48 +0300

    added review functionality

--- movie/models.py
+++ movie/models.py
@@ -1,6 +1,7 @@
 from distutils.command.upload import upload
 from email.mime import image
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Movie(models.Model):
@@ -11,3 +12,13 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+class Review(models.Model):
+    text = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
+    user =  models.ForeignKey(User,on_delete=models.CASCADE)
+    movie = models.ForeignKey( Movie,on_delete=models.CASCADE)
+    watchAgain = models.BooleanField()
+ 
+    def __str__(self):
+        return self.text
