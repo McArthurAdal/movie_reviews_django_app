3cc6bfb6579d812382cb1f7e1d5b2275957b2d6f
Mark Kyama <mark.kyama.adal@gmail.com>
2022-08-02 10:26:25 +0300

    final

--- movie/views.py
+++ movie/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from .models import Movie , Review
 from django.shortcuts import get_object_or_404, redirect
 from .forms import ReviewForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def home(request):
@@ -17,14 +18,18 @@ def home(request):
 def about(request):
     return HttpResponse('<h1>Welcome to About Page<h1>')
 
+
 def signup(request):
     email = request.GET.get('email')
     return render(request, 'signup.html', {'email':email})
 
 def detail(request, movie_id):
     movie = get_object_or_404(Movie,pk=movie_id)
-    return render(request,'detail.html', {'movie':movie})
+    reviews = Review.objects.filter(movie = movie)
+    return render(request,'detail.html', {'movie':movie,
+                                        'reviews': reviews})
 
+@login_required
 def createreview(request, movie_id):
     movie = get_object_or_404(Movie,pk=movie_id) 
     if request.method == 'GET':
@@ -41,3 +46,27 @@ def createreview(request, movie_id):
             return render(request, 'createreview.html', 
                         {'form':ReviewForm(),
                         'error':'bad data passed in'})
+
+@login_required                   
+def updatereview(request, review_id):
+    review = get_object_or_404(Review,pk=review_id,user=request.user)
+    if request.method == 'GET':
+        form = ReviewForm(instance=review)
+        return render(request, 'updatereview.html',  {'review': review,'form':form})
+    else:
+        try:
+            form = ReviewForm(request.POST, 
+            instance=review)
+            form.save()
+            return redirect('detail', review.movie.id)
+        except ValueError:
+            return render(request,'updatereview.html',
+            {'review': review,
+            'form':form,
+            'error':'Bad data in form'})
+
+@login_required
+def deletereview(request, review_id):
+    review = get_object_or_404(Review, pk=review_id, user=request.user)
+    review.delete()
+    return redirect('detail', review.movie.id)
